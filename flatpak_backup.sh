582ad993f814e335066b688498bd450f6bf6de50
nicknamenamenick <121328689+nicknamenamenick@users.noreply.github.com>
2023-03-23 10:56:21 -0400

    Add files via upload

--- flatpak_backup.sh
+++ flatpak_backup.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+flatpak list | awk '{print $2}' > packages.txt
+xargs -a /var/home/larks/Documents/Creative/packages2.txt flatpak install
+SRCDIR="/var/home/larks/.var/app"
+DESTDIR="/var/home/larks/"
+FILENAME=flatpakbackup-$(date +%-Y%-m%-d)-$(date +%-T).tgz 
+tar --create --gzip --file=$DESTDIR$FILENAME $SRCDIR
