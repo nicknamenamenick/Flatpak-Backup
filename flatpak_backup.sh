b15f249f6fbb217168f4227c1a2b1ad1d5b0da2c
nicknamenamenick <121328689+nicknamenamenick@users.noreply.github.com>
2023-03-26 22:28:03 -0400

    Delete flatpak_backup.sh

--- flatpak_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-flatpak list | awk '{print $2}' > packages.txt
-xargs -a /var/home/larks/Documents/Creative/packages2.txt flatpak install
-SRCDIR="/var/home/larks/.var/app"
-DESTDIR="/var/home/larks/"
-FILENAME=flatpakbackup-$(date +%-Y%-m%-d)-$(date +%-T).tgz 
-tar --create --gzip --file=$DESTDIR$FILENAME $SRCDIR
